91ac91ac318b720ffe6843b39258dd829ed7ac77
a <b>
2023-03-14 18:32:51 +0900

    なんかコンフリクト時のミスコミットを修正

--- docker_up_run.sh
+++ docker_up_run.sh
@@ -1,7 +1,4 @@
 docker rm -f discord
 docker run -v "$PWD/src:/gpt-discord-bot/" -d \
         --name discord -it gpt-discord-bots
-<<<<<<< Updated upstream
-=======
-docker logs discord -f
->>>>>>> Stashed changes
+# docker logs discord -f
